01b65530c79c1be638d9b7aac2f66e7e253e9bbb
shivani909 <shivanipandeycontact@gmail.com>
2025-04-10 02:32:55 +0530

    binary_nums

--- code.cpp
+++ code.cpp
@@ -2,216 +2,249 @@
 
 using namespace std;
 
-int main()
-{
+// using nested loops to print a patterns
 
-    // using nested loops to print a patterns
+// int n = 6;
 
-    // int n = 6;
+// for (int i = 0; i < n; i++)
+// {
 
-    // for (int i = 0; i < n; i++)
-    // {
+//     char ch = 'A';
 
-    //     char ch = 'A';
+//     for (int j = 0; j < n; j++)
+//     {
+//         cout << ch << " ";
+//         ch = ch + 1;
+//     }
 
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         cout << ch << " ";
-    //         ch = ch + 1;
-    //     }
+//     cout << endl;
+// }
 
-    //     cout << endl;
-    // }
+// using nested loops to print a patterns
+//    int n = 3;
 
-    // using nested loops to print a patterns
-    //    int n = 3;
+//    int num = 1;
 
-    //    int num = 1;
+//    for (int i = 0; i < n ; i++) {
 
-    //    for (int i = 0; i < n ; i++) {
+//     for (int j = 0; j < n; j++) {
+//         cout << num << " ";
+//         num++;
+//     }
 
-    //     for (int j = 0; j < n; j++) {
-    //         cout << num << " ";
-    //         num++;
-    //     }
+//     cout << endl;
 
-    //     cout << endl;
+//    }
 
-    //    }
+// cout << "after pattern : " << num << endl;
 
-    // cout << "after pattern : " << num << endl;
+// PB 3
 
-    // PB 3
+// int n = 3;
+//  char ch = 'A';
 
-    // int n = 3;
-    //  char ch = 'A';
+//  for ( int i = 0; i < n; i++)
+//  {
+//    for (int j = 0; j <n ; j++)  {
+// cout << ch << " ";
+// ch++;
+//    }
 
-    //  for ( int i = 0; i < n; i++)
-    //  {
-    //    for (int j = 0; j <n ; j++)  {
-    // cout << ch << " ";
-    // ch++;
-    //    }
+//     cout << endl;
+//  }
 
-    //     cout << endl;
-    //  }
+// PB4 (tRiangle pattern)
 
-    // PB4 (tRiangle pattern)
+// int n = 6;
 
-    // int n = 6;
+// for (int i = 1; i <= n; i++)
+// {
+//     for (int j = 1; j <= i; j++)
+//     {
 
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     for (int j = 1; j <= i; j++)
-    //     {
+//         cout << "*" << " ";
+//     }
+//     cout << endl;
+// }
 
-    //         cout << "*" << " ";
-    //     }
-    //     cout << endl;
-    // }
+// PB5 (tRiangle pattern 2)
 
-    // PB5 (tRiangle pattern 2)
+// int n = 4;
 
-    // int n = 4;
+// for(int i =1; i<=n ; i++) { // outer loop ( will run n times)
+//     for(int j =1; j <= i; j++) { // inner loop
+//         cout << i << " ";
+//     }
+//     cout << endl;
 
-    // for(int i =1; i<=n ; i++) { // outer loop ( will run n times)
-    //     for(int j =1; j <= i; j++) { // inner loop
-    //         cout << i << " ";
-    //     }
-    //     cout << endl;
+// }
 
-    // }
+// PB6 (tRiangle pattern 3)
 
-    // PB6 (tRiangle pattern 3)
+// char input, alphabet = 'A';
+// int i, j;
 
-    // char input, alphabet = 'A';
-    // int i, j;
+// cout << "Enter the uppercase character you want to print in the last row:";
+// cin >> input;
 
-    // cout << "Enter the uppercase character you want to print in the last row:";
-    // cin >> input;
+// // outer loop is responsible for rows
+// for (int i = 1; i <= (input - 'A' + 1); i++)
+// {
 
-    // // outer loop is responsible for rows
-    // for (int i = 1; i <= (input - 'A' + 1); i++)
-    // {
+//     // inner loop is responsible for columns
+//     for (int j = 1; j <= i; j++)
+//     {
+//         cout << alphabet << " ";
+//     }
+//     alphabet++;
 
-    //     // inner loop is responsible for columns
-    //     for (int j = 1; j <= i; j++)
-    //     {
-    //         cout << alphabet << " ";
-    //     }
-    //     alphabet++;
+//     // give line breaks after ending every row
+//     cout << "\n";
+// }
 
-    //     // give line breaks after ending every row
-    //     cout << "\n";
-    // }
+// PB7 (tRiangle pattern 4)
 
-    // PB7 (tRiangle pattern 4)
+// int n = 4;
 
-    // int n = 4;
+// for (int i = 0; i <= n; i++)
+// {
 
-    // for (int i = 0; i <= n; i++)
-    // {
+//     for (int j = 1; j <= i; j++)
+//     {
+//         cout << j << " "; /* code */
+//     }
+//     cout << endl;
+// }
 
-    //     for (int j = 1; j <= i; j++)
-    //     {
-    //         cout << j << " "; /* code */
-    //     }
-    //     cout << endl;
-    // }
+// PB8 (reverse tRiangle pattern 5)
 
-    // PB8 (reverse tRiangle pattern 5)
+// int n = 4;
 
-    // int n = 4;
+// for (int i = 0; i < n; i++)
+// {
 
-    // for (int i = 0; i < n; i++)
-    // {
+//     for (int j = i + 1; j > 0; j--)
+//     {
+//         cout << j;
+//     }
+//     cout << endl;
+// }
 
-    //     for (int j = i + 1; j > 0; j--)
-    //     {
-    //         cout << j;
-    //     }
-    //     cout << endl;
-    // }
+// PB9 (Inverted tRiangle pattern 6)
 
-    // PB9 (Inverted tRiangle pattern 6)
+// int n = 4;
 
-    // int n = 4;
+// int num = 1;
+// for (int i = 0; i <= n; i++)
 
-    // int num = 1;
-    // for (int i = 0; i <= n; i++)
+// {
 
-    // {
+//     // cout << i << " ";
+//     //     spaces
+//     for (int j = 0; j < i; j++)
+//     {
+//         cout << j;
+//     }
 
-    //     // cout << i << " ";
-    //     //     spaces
-    //     for (int j = 0; j < i; j++)
-    //     {
-    //         cout << j;
-    //     }
+// // nums
 
-        // // nums
+//     for (int j = 0; j < n - i; j++)
+//     {
 
-    //     for (int j = 0; j < n - i; j++)
-    //     {
+//         cout << (i + 1);
+//     }
 
-    //         cout << (i + 1);
-    //     }
+//     cout << endl;
+// }
 
-    //     cout << endl;
-    // }
+// int n = 3;
 
+// for(int i = 1; i <= n; i++) {
 
-    // int n = 3;
-
-
-    // for(int i = 1; i <= n; i++) {
-        
-    //    for(int j =1 ; j <= i ; j++) {
-    //         cout << j << " ";
-    //    }
-    //    cout << endl;
-    // }
+//    for(int j =1 ; j <= i ; j++) {
+//         cout << j << " ";
+//    }
+//    cout << endl;
+// }
 
 //     int n = 3;
 //     int num = 1;
-    
-    
 
 //     for (int i = 1; i <= n; i++)
 //     {
 
-       
 //        for(int j = 1; j <=i; j++) {
 //             cout << num << " ";
 //             num++;
-           
+
 //         }
 
 // cout << endl;
-   
-    
-
 
 // }
 
 // Reversed Floyd’s Triangle
-int n = 5;
-int num = n * (n + 1) / 2; // formula to find the sum of n natural numbers
+// int n = 5;
+// int num = n * (n + 1) / 2; // formula to find the sum of n natural numbers
 
-for (int i = n; i >= 1; i--)
-{
+// for (int i = n; i >= 1; i--)
+// {
 
-    
-   for(int j = 1; j <= i; j++) {
-        cout << num << " ";
-        num--;
-       
-       
-    }
-    cout << endl;
+//    for(int j = 1; j <= i; j++) {
+//         cout << num << " ";
+//         num--;
 
-} 
+//     }
+//     cout << endl;
 
-//
+// }
 
-}
+// convert decimal to binary.
+
+// int decToBinary (int decNum) {
+//     int ans = 0;
+//     int pow = 1;
+
+//     while (decNum > 0) {
+//         int rem = decNum % 2;
+//          decNum /= 2;
+
+//          ans += rem * pow;
+//          pow *= 10;
+//     }
+//     return ans;
+
+// }
+// int main () {
+//     int decNum = 50;
+
+// cout << "Decimal Number: " << decToBinary(decNum) << endl;
+// return 0;
+// }
+
+// convert binary to decimal
+
+// int binarytoDec(int num)
+// {
+
+//     int value = 0;
+//     int pow = 1;
+
+//     while (num > 0)
+//     {
+//         int rem = num % 10; // remainder
+
+//         value += rem * pow; // sum
+//         pow *= 2;           // update power of 2
+//         num /= 10;          // // update num
+//     }
+//     return value;
+// }
+
+// int main()
+// {
+//     int num = 101010;
+
+//     cout << binarytoDec(num) << endl;
+//     return 0;
+// }
